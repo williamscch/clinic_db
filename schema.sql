8353e342d9f3b4e5cdfb7be862a3d1021af2bfc2
wdavidcch <david.13.2121@gmail.com>
2022-09-21 16:34:56 -0400

    Add structure of the tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,50 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE DATABASE clinic_db;
+
+
+CREATE TABLE patients(
+    id INT NOT NULL generated always as identity,
+    name varchar(100) NOT NULL,
+    date_of_birth date,
+    primary key(id)
+);
+
+
+CREATE TABLE medical_histories(
+    id INT NOT NULL generated always as identity,
+    admitted_at timestamp,
+    patient_id int NOT NULL,
+    status varchar(100) NOT NULL,
+    primary key(id),
+    foreign key(patient_id) references patients(id)
+);
+
+CREATE TABLE treatments(
+    id INT NOT NULL generated always as identity,
+    type varchar(100) NOT NULL,
+    name varchar(100) not null,
+    primary key(id)
+);
+
+CREATE TABLE invoices(
+    id INT NOT NULL generated always as identity,
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history__id int, 
+    primary key(id),
+    foreign key(medical_history__id) references medical_histories(id)
+);
+
+CREATE TABLE invoice_items(
+    id INT NOT NULL generated always as identity,
+    unit_price decimal,
+    quantity int,
+    total_price decimal,
+    invoice_id int,
+    treatment_id int,
+    primary key(id),
+    foreign key(invoice_id) references invoices(id),
+    foreign key(treatment_id) references treatments(id)
 );
